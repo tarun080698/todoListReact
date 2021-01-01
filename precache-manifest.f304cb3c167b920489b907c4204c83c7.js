2575a3613709fae8be56740ec266ded120d41449
tarun080698 <tarun3dadlani@gmail.com>
2021-01-01 16:55:26 +0530

    Updates

---
+++ precache-manifest.f304cb3c167b920489b907c4204c83c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57cc70c04cae0fdb12fb2993dd8fe5dd",
+    "url": "/todoListReact/index.html"
+  },
+  {
+    "revision": "21ee794d0752d287b8d4",
+    "url": "/todoListReact/static/css/main.128cac5b.chunk.css"
+  },
+  {
+    "revision": "e432fdb767ef73966b22",
+    "url": "/todoListReact/static/js/2.5ca81efd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoListReact/static/js/2.5ca81efd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ee794d0752d287b8d4",
+    "url": "/todoListReact/static/js/main.d2b6e89d.chunk.js"
+  },
+  {
+    "revision": "7d8187fe90787a5306bb",
+    "url": "/todoListReact/static/js/runtime-main.941e3fcd.js"
+  }
+]);
